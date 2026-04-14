f4ea4b1eb7bffa374c5db02c403b405b2ec1b9d9
Fujennai <57229680+Fujennai@users.noreply.github.com>
2026-04-14 13:17:08 +0200

    Update app.py

--- app.py
+++ app.py
@@ -60,7 +60,7 @@ with tab:
         "Mínimo de partidas",
         min_value=0,
         max_value=int(df_main["Partidas"].max()),
-        value=50,
+        value=0,
         step=10
     )
     df_main_filtered = df_main[df_main["Partidas"] >= min_partidas]
